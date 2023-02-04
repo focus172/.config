8b33ac5461ad81d39a021184081de0e131de9313
Evan Stokdyk <evan.stokdyk@gmail.com>
2023-02-04 10:57:15 -0800

    Add initial mac files

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,41 @@
+-- ------------ --
+-- mappings.lua --
+-- @Focus172    --
+-- ------------ --
+
+local opts = { silent = true, noremap = false }
+
+-- setting leader key and mapping it to do nothing
+vim.g.mapleader = " "
+
+-- setting key maps --
+vim.api.nvim_set_keymap("", "Y", "y$", opts)
+-- vim.api.nvim_set_keymap("", "<leader>e", ":Ex<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>s", ":w<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leaader>q", ":q<CR>", opts)
+
+--vim.api.nvim_set_keymap("n", "<TAB>", ":BufferLineCycleNext<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferLineCyclePrev<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<leader>x", ':bd<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<leader>c", "<cmd> :lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", opts)
+--vim.api.nvim_set_keymap("n", "<leader>n", ":NnnPicker<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<leader>N", ":NnnExplorer<CR>", opts)
+
+--vim.api.nvim_set_keymap("v", "<leader>c", "<esc><cmd> :lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
+--vim.api.nvim_set_keymap("v", "<leader>a", ":EasyAlign<CR>", opts)
+
+--vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+--vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<leader>q", ':lua require"dap".close()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<leader>d", ':lua require"dapui".toggle({ reset = true })<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<leader>b", ':lua require"dap".toggle_breakpoint()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<leader>e", ':lua require("dapui").eval()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<C-c>", ':lua require"dap".continue()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<C-s>", ':lua require"dap".step_into()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<C-o>", ':lua require"dap".step_over()<CR>', opts)
+--vim.api.nvim_set_keymap("n", "<C-Left>", "<C-w>h", opts)
+--vim.api.nvim_set_keymap("n", "<C-Down>", "<C-w>j", opts)
+--vim.api.nvim_set_keymap("n", "<C-Up>", "<C-w>k", opts)
+--vim.api.nvim_set_keymap("n", "<C-Right>", "<C-w>l", opts)
+--vim.api.nvim_set_keymap("n", "<leader>t", ":TroubleToggle<CR>", opts)
