0c8bce0cfa26962e18a59bbc07179859b6edc694
Focus172 <evan.stokdyk@gmail.com>
2023-05-25 18:15:10 -0700

    more nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,23 +4,14 @@
   home.username = "focus";
   home.homeDirectory = "/home/focus";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
     pkgs.exa
     pkgs.gitui
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.starship
+    pkgs.bat
+    pkgs.alejandra
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -56,10 +47,6 @@
   #
   #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   #
-  # or
-  #
-  #  /etc/profiles/per-user/focus/etc/profile.d/hm-session-vars.sh
-  #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -67,4 +54,16 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "22.11"; # Please read the comment before changing.
+
+
 }
--- nvim/lua/custom/conf/nullls.lua
+++ nvim/lua/custom/conf/nullls.lua
@@ -4,7 +4,7 @@ local options = {
     sources = {
         null_ls.builtins.formatting.rustfmt,
         -- null_ls.builtins.formatting.brittany, -- haskell
-        -- null_ls.builtins.formatting.nixfmt, -- nix
+        null_ls.builtins.formatting.alejandra, -- nix
         null_ls.builtins.formatting.gofmt, -- go
         -- null_ls.builtins.formatting.ktlint, -- kotlin
         null_ls.builtins.formatting.lua_format, -- lua
